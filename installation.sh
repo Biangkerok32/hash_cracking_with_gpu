87d8f29640d5aea07587cd59c34457c9a4194bb4
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2022-10-02 20:55:58 +0700

    Update installation.sh
    update last hashcat bunary - hashcat-6.2.6

--- installation.sh
+++ installation.sh
@@ -2,10 +2,10 @@
 #Installation script
 
 cd ../content/gdrive/My\ Drive/
-wget https://hashcat.net/files/hashcat-5.1.0.7z
+wget https://hashcat.net/files/hashcat-6.2.6.7z
 sudo apt install p7zip
-sudo p7zip -d hashcat-5.1.0.7z
-cd hashcat-5.1.0
+sudo p7zip -d hashcat-6.2.6.7z
+cd hashcat-6.2.6
 sudo cp hashcat64.bin /usr/bin/
 sudo ln -s /usr/bin/hashcat64.bin /usr/bin/hashcat	
 sudo cp -Rv OpenCL/ /usr/bin/
